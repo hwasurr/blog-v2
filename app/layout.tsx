ea0a0a8578c7c5dfac5704b4c3922ca04199377c
hwasurr <iamsupermazinga@gmail.com>
2024-01-30 22:44:37 +0900

    feat: vercel analytics 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
           {children}
           <SiteFooter></SiteFooter>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );

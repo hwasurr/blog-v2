e87519a2e9bb02023000a5e3d955c6d9d6bcc8b1
hwasurr <iamsupermazinga@gmail.com>
2024-01-30 22:58:15 +0900

    feat: 기존 컨텐츠 영구 리다이렉트 추가

--- next.config.js
+++ next.config.js
@@ -11,6 +11,56 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      // 이전 버전 블로그 콘텐츠 영구 리디렉션 처리
+      {
+        source: '/api/graphql-example',
+        destination: '/blog/api/graphql-example',
+        permanent: true,
+      },
+      {
+        source: '/api/rest-graphql-differences',
+        destination: '/blog/api/rest-graphql-differences',
+        permanent: true,
+      },
+      {
+        source: '/AWS/architecting-on-aws',
+        destination: '/blog/AWS/architecting-on-aws',
+        permanent: true,
+      },
+      {
+        source: '/AWS/infrastructure-as-code',
+        destination: '/blog/AWS/infrastructure-as-code',
+        permanent: true,
+      },
+      {
+        source: '/AWS/microservice-with-ecs',
+        destination: '/blog/AWS/microservice-with-ecs',
+        permanent: true,
+      },
+      {
+        source: '/git-github/git-manage',
+        destination: '/blog/git-github/git-manage',
+        permanent: true,
+      },
+      {
+        source: '/git-github/github-actions',
+        destination: '/blog/git-github/github-actions',
+        permanent: true,
+      },
+      {
+        source: '/nesetjs/caching',
+        destination: '/blog/nesetjs/caching',
+        permanent: true,
+      },
+      {
+        source: '/redis/cluster',
+        destination: '/blog/redis/cluster',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

2a928507c903470625230a22121823f228fd5378
hwasurr <iamsupermazinga@gmail.com>
2024-02-01 22:26:16 +0900

    feat: speed insights 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
           <SiteFooter></SiteFooter>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
